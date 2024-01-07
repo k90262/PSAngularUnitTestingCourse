c489a9c0ff0540537761f34f3870ebded8fcbad5
Bill Ho <ycho.bill.lab@gmail.com>
2024-01-08 00:23:17 +0800

    [Shallow Integration Tests] NativeElement vs. DebugElement

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {HeroComponent} from "./hero.component";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe("HeroComponent (shallow test)", () => {
   let fixture: ComponentFixture<HeroComponent>;
@@ -23,6 +24,9 @@ describe("HeroComponent (shallow test)", () => {
     fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
     fixture.detectChanges();
 
-    expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+    let deA = fixture.debugElement.query(By.css('a'));
+    expect(deA.nativeElement.textContent).toContain('SuperDude');
+
+    //expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
   });
 });
